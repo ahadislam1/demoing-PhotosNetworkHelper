162eaddb0524287edfce66f5e6daadf5a24f0fb2
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-12-09 12:08:52 -0500

    Initial Commit

---
+++ AstronomyPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AstronomyPhotos
+//
+//  Created by Ahad Islam on 12/9/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
